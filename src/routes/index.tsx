758469b83b80df17a799e9f5c8ef8adc046814bd
Lucas Rodrigues <ha-RodriLuc01@electrolux.com>
2025-09-24 14:23:18 -0300

    remove home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Quiz from '../pages/quiz';
-import Home from '../pages/home';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Quiz />} />
       <Route path="/quiz" element={<Quiz />} />
     </Routes>
   );
